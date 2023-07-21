71f292b3ca2d2b300a354300a34d841081f41555
Object626 <Shtokov6161@gmail.com>
2023-07-22 01:58:32 +0400

    Добавил фильмы актёров и тд

--- films/movies/admin.py
+++ films/movies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Movie, MovieShots, Actor, RatingStar, Rating, Reviews, Genre
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Movie)
+admin.site.register(MovieShots)
+admin.site.register(Actor)
+admin.site.register(Rating)
+admin.site.register(Reviews)
+admin.site.register(RatingStar)
+admin.site.register(Genre)
+
